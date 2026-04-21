37215c4cd25021565eccdce1f8a115a5b0689280
ella <randomgive98@gmail.com>
2026-04-21 13:08:04 +0100

    01-03

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,8 @@
 # How many new outputs were created by block 243,825?
+block_height=243825
+block_hash=$(bitcoin-cli -signet getblockhash $block_height)
+
+outputs=$(bitcoin-cli -signet getblock $block_hash 2 | jq '[.tx[].vout | length] | add')
+
+echo $outputs
+block_hash=$0000000e120ff9801366e9c75f1fbe6b72cc6a577bf16a402fd1521c3653f24d
--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,13 @@
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
+coinbase_blockhash=$(bitcoin-cli -signet getblockhash 216128)
+coinbase_txid=$(bitcoin-cli -signet getblock $coinbase_blockhash 2 | jq -r '.tx[0].txid')
+
+spending_blockhash=$(bitcoin-cli -signet getblockhash 216351)
+
+spending_blocktx=$(bitcoin-cli -signet getblock $spending_blockhash 2 | jq -r --arg cb "$coinbase_txid" '
+  .tx[]
+  | select(any(.vin[]?; .txid == $cb))
+  | .txid
+')
+
+echo "$spending_blocktx"
