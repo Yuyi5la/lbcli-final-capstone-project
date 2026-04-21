a35417d2bee24e572f988f078792c40299dac30b
ella <randomgive98@gmail.com>
2026-04-21 16:25:24 +0100

    fix 07

--- submission/07.sh
+++ submission/07.sh
@@ -3,6 +3,6 @@
 block_height=243834
 block_hash=$(bitcoin-cli -signet getblockhash $block_height)
 
-coinbase_tx=$(bitcoin-cli -signet getblock $block_hash 2 | jq -r '.tx[0]')
+coinbase_txid=$(bitcoin-cli -signet getblock $block_hash 1 | jq -r '.tx[0]')
 
-echo $coinbase_tx
+echo $coinbase_txid
